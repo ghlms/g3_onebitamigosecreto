62cc1ea0a7935cb6bc13d11b0a47e465e87054ca
Gustavo Serafim <ghlm.serafim@gmail.com>
2019-02-20 15:57:45 -0300

    set campaing_Mailer

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -1,2 +1,9 @@
-class RafflesMailer < ApplicationMailer
+class CampaignMailer < ApplicationMailer
+
+  def raffle(campaign, member, friend)
+    @campaign = campaign
+    @member = member
+    @friend = friend
+    mail to: @member.email, subject: "Nosso Amigo Secreto: #{@campaign.title}"
+  end
 end
